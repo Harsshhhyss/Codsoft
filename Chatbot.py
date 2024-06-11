3cc8b3b6aae40cc40493cc28246a5c0615e80d2e
Harsh Kumar <168293998+Harsshhhyss@users.noreply.github.com>
2024-06-12 00:55:48 +0530

    A Rule-Based Chatbot

---
+++ Chatbot.py
@@ -0,0 +1,90 @@
+import nltk
+from nltk.chat.util import Chat, reflections
+#Pairs is a list of patterns and responses.
+pairs = [
+    [
+        r"(.*)Robot",
+        ["hi Human, how can I help you "]
+    ],
+    [
+        r"(.*)question",
+        ["I am here for your help"]
+    ],
+    
+    [
+        r"(.*)developed ",
+        ["Harsh created me using Python"]
+    ],
+    [
+        r"(.*)alive ",
+        ["I am Robot, I am just a computer program . Created by Harsh"]
+    ],
+    
+    
+    [
+        r"(.*)name(.*)",
+        ["Hello %2, How are you today ?",]
+    ],
+    [
+        r"(.*)help(.*) ",
+        ["I can help you ",]
+    ],
+     [
+        r"(.*) your name ?",
+        ["My name is AI Robot, but you can just call me robot and I'm a chatbot .",]
+    ],
+    [
+        r"how are you (.*) ?",
+        ["I'm doing very well", "i am great !"]
+    ],
+    [
+        r"sorry (.*)",
+        ["Its alright","Its OK, never mind that",]
+    ],
+    [
+        r"i'm (.*) (good|well|okay|ok)",
+        ["Nice to hear that","Alright, great !",]
+    ],
+    [
+        r"(hi|hey|hello|hola|holla)(.*)",
+        ["Hello", "Hey there",]
+    ],
+    [
+        r"what (.*) want ?",
+        ["Make me an offer I can't refuse",]
+        
+    ],
+    [
+        r"(.*)created(.*)",
+        ["Harsh created me using Python's NLTK library ","Its top secret........... ;)",]
+    ],
+    [
+        r"how (.*) health (.*)",
+        ["Health is very important, but I am a computer, so I don't need to worry about my health ",]
+    ],
+    [
+        r"(.*)(sports|game|sport)(.*)",
+        ["I'm a very big fan of Cricket",]
+    ],
+    [
+        r"who (.*) (Cricketer|Batsman)?",
+        ["Virat Kohli"]
+    ],
+    [
+        r"quit",
+        ["Bye for now. See you soon :) ","It was nice talking to you. See you soon :)"]
+    ],
+    [
+        r"(.*)",
+        ['That is nice to hear']
+    ],
+    
+]
+
+my_dummy_reflections= {
+    "go"     : "gone",
+    "hello"    : "hey there"
+}
+chat = Chat(pairs, reflections)
+chat.converse()
+print("Chatbot: Hi! I'm a simple chatbot,I'm here to assist you!")
